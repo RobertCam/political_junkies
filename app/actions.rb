7db1432421b37b1958f13385c546e83777a4913b
RobertCam <rob.cameron85@gmail.com>
2015-10-22 21:57:37 +0000

    updated nav-bar, added condidate ids

--- app/actions.rb
+++ app/actions.rb
@@ -29,32 +29,13 @@ SANDERS = 'N00000528'
 #INDEPENDANT CANDIDATES
 STEIN = 'N00033776'
 
-helpers do 
-
-end
-
-helpers do
-
-  def extract_amounts(cand_type)
-    total = 0
-    sum_pac = 0
-    sum_indiv = 0
-    
-    cand_type.each do |org|
-      total += org["total"].to_i
-      sum_pac += org["pacs"].to_i 
-      sum_indiv += org["indivs"].to_i
-    end
-    [sum_pac, sum_indiv, total]
-end
-
 #landing page (aka home page)
 get '/' do
   erb :index
 end
 
 #shows a specific candidate
-get '/candidates/:id/show' do
+get '/candidates/show/:id' do
 
   candidate = OpenSecrets::Candidate.new
   cand_sum = candidate.summary({:cid => params[:id]})["response"]
@@ -62,24 +43,17 @@ get '/candidates/:id/show' do
 
   cand_con = candidate.contributors({:cid => params[:id]})["response"]
   @cand_con = cand_con["contributors"]
-  @cand_con_extract = extract_amounts(@cand_con["contributor"])
 
   cand_ind = candidate.industries({:cid => params[:id]})["response"]
   @cand_ind = cand_ind["industries"]
-  @cand_ind_extract = extract_amounts(@cand_ind["industry"])
 
   cand_sec = candidate.sector({:cid => params[:id]})["response"]
   @cand_sec = cand_sec["sectors"]
-  @cand_sec_extract = extract_amounts(@cand_sec["sector"])
-  
-  erb :'candidates/show'
 
+  erb :'candidates/show'
 end
 
 #shows all the candidates
 get '/candidates' do
   erb :'candidates/index'
-end
-
-
 end
