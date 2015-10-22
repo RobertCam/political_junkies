f0464e257c63fefb464ff64f4d63a991e09b9860
vagrant <vagrant@lighthouse-labs>
2015-10-22 20:09:51 +0000

    Added $ donated for each of the major SPONSOR types

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,21 @@
 #just to test for getting data on the 'candidates/show erb'
 CID = 'N00007360'
 
+helpers do
+
+  def extract_amounts(cand_type)
+    total = 0
+    sum_pac = 0
+    sum_indiv = 0
+    
+    cand_type.each do |org|
+      total += org["total"].to_i
+      sum_pac += org["pacs"].to_i 
+      sum_indiv += org["indivs"].to_i
+    end
+    [sum_pac, sum_indiv, total]
+end
+
 #landing page (aka home page)
 get '/' do
   erb :index
@@ -10,22 +25,30 @@ end
 get '/candidates/show' do
 
   candidate = OpenSecrets::Candidate.new
+
   cand_sum = candidate.summary({:cid => CID})["response"]
   @cand_sum = cand_sum["summary"]
 
   cand_con = candidate.contributors({:cid => CID})["response"]
   @cand_con = cand_con["contributors"]
+  @cand_con_extract = extract_amounts(@cand_con["contributor"])
 
   cand_ind = candidate.industries({:cid => CID})["response"]
   @cand_ind = cand_ind["industries"]
+  @cand_ind_extract = extract_amounts(@cand_ind["industry"])
 
   cand_sec = candidate.sector({:cid => CID})["response"]
   @cand_sec = cand_sec["sectors"]
-
+  @cand_sec_extract = extract_amounts(@cand_sec["sector"])
+  
   erb :'candidates/show'
+
 end
 
 #shows all the candidates
 get '/candidates' do
   erb :'candidates/index'
+end
+
+
 end
